7911f89a2fecf41d3781c1af365c1138e2c1b488
jeffgourlay <jeffgourlay@gmail.com>
2021-02-03 01:41:06 -0500

    Ready to commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route,
+         Switch,
+         useParams
+       } from 'react-router-dom';
+import NoMatch from 'react-router-nomatch';
 import './App.css';
+//*** Import the Welcome component ***//
+import Welcome from './components/Welcome/Welcome';
+//*** Import the clock */
+import Clock from './components/Clock/Clock';
+//*** Import the contact component */
+import Contact from './components/Contact/Contact';
+//*** Import the navigation component */
+import Navigation from './components/Navigation/Navigation';
+//*** Import the 404 component */
+import NotFound from './components/NotFound/NotFound';
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/* Adding the Welcome component and passing the value "Eric" to the "name" prop */}
+          <Navigation />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => <Welcome {...props} name="Jeff" />}
+            />
+            <Route 
+              path="/welcome/:name" 
+              render={(props) => <Welcome {...props} name={props.match.params.name}/>}
+            />
+            <Route path="/clock" component={Clock} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+function GetName() {
+  let {name} = useParams();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {name}
   );
 }
 
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function NotFound(props) {
+    return (
+        <div>
+            <h1>To paraphrase Obi-Wan Kenobi: This is not the page you are looking for.  Move along.</h1>
+        </div>
+    );
+}
+export default NotFound;
